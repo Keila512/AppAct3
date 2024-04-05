1c77eb32abdce4a6298481a3b84ad50663cbdaac
Keila De la torre solis <keila.solis.123@outlook.com>
2024-04-04 18:40:32 -0600

    Agregar archivos de proyecto.

---
+++ AppAct3/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AppAct3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertJSON = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnConvertXML = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnConvertCSV = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConvertJSON
+            // 
+            this.btnConvertJSON.Location = new System.Drawing.Point(45, 131);
+            this.btnConvertJSON.Name = "btnConvertJSON";
+            this.btnConvertJSON.Size = new System.Drawing.Size(180, 26);
+            this.btnConvertJSON.TabIndex = 0;
+            this.btnConvertJSON.Text = "CONVERT JSON";
+            this.btnConvertJSON.UseVisualStyleBackColor = true;
+            this.btnConvertJSON.Click += new System.EventHandler(this.btnConvertJSON_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(279, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(834, 540);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnConvertXML
+            // 
+            this.btnConvertXML.Location = new System.Drawing.Point(45, 180);
+            this.btnConvertXML.Name = "btnConvertXML";
+            this.btnConvertXML.Size = new System.Drawing.Size(180, 26);
+            this.btnConvertXML.TabIndex = 2;
+            this.btnConvertXML.Text = "CONVERT XML";
+            this.btnConvertXML.UseVisualStyleBackColor = true;
+            this.btnConvertXML.Click += new System.EventHandler(this.btnConvertXML_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(79, 35);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(122, 23);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "OPEN";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnConvertCSV
+            // 
+            this.btnConvertCSV.Location = new System.Drawing.Point(45, 85);
+            this.btnConvertCSV.Name = "btnConvertCSV";
+            this.btnConvertCSV.Size = new System.Drawing.Size(180, 27);
+            this.btnConvertCSV.TabIndex = 4;
+            this.btnConvertCSV.Text = "CONVERT CSV";
+            this.btnConvertCSV.UseVisualStyleBackColor = true;
+            this.btnConvertCSV.Click += new System.EventHandler(this.btnConvertCSV_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1175, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(520, 540);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1722, 718);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnConvertCSV);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnConvertXML);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnConvertJSON);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertJSON;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnConvertXML;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnConvertCSV;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AppAct3/Form1.cs
@@ -0,0 +1,297 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace AppAct3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Files CSV (*.csv)|*.csv|Files JSON (*.json)|*.json|Files XML (*.xml)|*.xml";
+            openFileDialog.Title = "Select Files";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePathCsv = openFileDialog.FileName;
+                string filePathXml = openFileDialog.FileName;
+                string filePathJson = openFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePathCsv);
+
+                switch (fileExtension)
+                {
+                    case ".csv":
+                        filePathCsv = openFileDialog.FileName;
+                        LoadCsv(filePathCsv);
+                        break;
+                    case ".json":
+                        try
+                        {
+                            string jsonContent = File.ReadAllText(filePathJson);
+                            richTextBox1.Text = jsonContent;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("ERROR" + ex.Message);
+                        }
+                        break;
+                    case ".xml":
+                        try
+                        {
+                            string xmlContent = File.ReadAllText(filePathXml);
+                            richTextBox1.Text = xmlContent;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("ERROR" + ex.Message);
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("PLEASE SELECT THE CORRECT FILE");
+                    break;
+                }
+            }
+        }
+        private void LoadCsv(string filePath)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            string[] lines = File.ReadAllLines(filePath);
+            // Add columns
+            string[] headers = lines[0].Split(',');
+            foreach (string header in headers)
+            {
+                dataGridView1.Columns.Add(header, header);
+            }
+            // Add rows
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] fields = lines[i].Split(',');
+                dataGridView1.Rows.Add(fields);
+            }
+        }
+        private void btnConvertCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Files JSON (*.json)|*.json|Files XML (*.xml)|*.xml";
+            openFileDialog.Title = "Select Files";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePath);
+
+                switch (fileExtension)
+                {
+                    case ".json":
+                        ConvertJSONtoCSV(filePath);
+                        break;
+                    case ".xml":
+                        ConvertXMLtoCSV(filePath);
+                        break;
+                    default:
+                        MessageBox.Show("PLEASE SELECT THE CORRECT FILE");
+                    break;
+                }
+            }
+        }
+        private void ConvertJSONtoCSV(string FilePathJson)
+        {
+            string FilePathCsv = Path.ChangeExtension(FilePathJson, ".csv");
+
+            using (StreamReader reader = new StreamReader(FilePathJson))
+            {
+                string json = reader.ReadToEnd();
+                DataTable dataTable = JsonConvert.DeserializeObject<DataTable>(json);
+
+                using (StreamWriter writer = new StreamWriter(FilePathCsv))
+                {
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                        writer.WriteLine(string.Join(",", fields));
+                    }
+                }
+            }MessageBox.Show("GENERATED CSV FILE" + FilePathCsv);
+        }
+        private void ConvertXMLtoCSV(string FilePathXml)
+        {
+            string FilePathCsv = Path.ChangeExtension(FilePathXml, ".csv");
+
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(FilePathXml);
+
+            using (StreamWriter writer = new StreamWriter(FilePathCsv))
+            {
+                foreach (DataTable table in dataSet.Tables)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        string[] fields = row.ItemArray.Select(field => field.ToString()).ToArray();
+                        writer.WriteLine(string.Join(",", fields));
+                    }
+                }
+            }MessageBox.Show("GENERATED CSV FILE" + FilePathCsv);
+        }
+        private void btnConvertJSON_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Files CSV (*.csv)|*.csv|Files XML (*.xml)|*.xml";
+            openFileDialog.Title = "Select Files";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePath);
+
+                switch (fileExtension)
+                {
+                    case ".csv":
+                        ConvertCSVtoJSON(filePath);
+                        break;
+                    case ".xml":
+                        ConvertXMLtoJSON(filePath);
+                        break;
+                    default:
+                        MessageBox.Show("PLEASE SELECT THE CORRECT FILE");
+                    break;
+                }
+            }
+        }
+        private void ConvertCSVtoJSON(string filePath)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string[] headers = reader.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(header);
+                }
+                while (!reader.EndOfStream)
+                {
+                    string[] rows = reader.ReadLine().Split(',');
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dataRow[i] = rows[i];
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+            string json = JsonConvert.SerializeObject(dataTable, Newtonsoft.Json.Formatting.Indented);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Files JSON (*.json)|*.json";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string FilePathJson = saveFileDialog.FileName;
+                File.WriteAllText(FilePathJson, json);
+            }
+        }
+        private void ConvertXMLtoJSON(string filePath)
+        {
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(filePath);
+            string json = JsonConvert.SerializeObject(dataSet, Newtonsoft.Json.Formatting.Indented);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Files JSON (*.json)|*.json";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string FilePathJson = saveFileDialog.FileName;
+                File.WriteAllText(FilePathJson, json);
+            }
+        }
+        private void btnConvertXML_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Files CSV (*.csv)|*.csv|Files JSON (*.json)|*.json";
+            openFileDialog.Title = "Select Files";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileExtension = Path.GetExtension(filePath);
+
+                string FilePathXml = Path.ChangeExtension(filePath, ".xml");
+
+                switch (fileExtension)
+                {
+                    case ".csv":
+                        ConvertCSVtoXML(filePath, FilePathXml);
+                        break;
+                    case ".json":
+                        ConvertJSONtoXML(filePath, FilePathXml);
+                        break;
+                    default:
+                        MessageBox.Show("PLEASE SELECT THE CORRECT FILE");
+                    break;
+                }
+            }
+        }
+        private void ConvertCSVtoXML(string FilePathCsv, string FilePathXml)
+        {
+            DataTable dataTable = ReadCSV(FilePathCsv);
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
+            dataSet.WriteXml(FilePathXml);
+        }
+        private void ConvertJSONtoXML(string FilePathJson, string FilePathXml)
+        {
+            string json = File.ReadAllText(FilePathJson);
+            DataTable dataTable = JsonConvert.DeserializeObject<DataTable>(json);
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add(dataTable);
+            dataSet.WriteXml(FilePathXml);
+        }
+        private DataTable ReadCSV(string filePath)
+        {
+            DataTable dataTable = new DataTable();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string[] headers = reader.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dataTable.Columns.Add(header);
+                }
+                while (!reader.EndOfStream)
+                {
+                    string[] rows = reader.ReadLine().Split(',');
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dataRow[i] = rows[i];
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }return dataTable;
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
